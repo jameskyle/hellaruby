85d9049566943e340cb53bbd5cdf74a9b760a6a7
James Kyle <jameskyle@ucla.edu>
2008-05-10 23:26:05 -0700

    Created methods for status calls to Hellanzb

--- app/models/hellanzb.rb
+++ app/models/hellanzb.rb
@@ -5,7 +5,7 @@ class Hellanzb
   def initialize 
     begin
       @server ||= XMLRPC::Client.new2(HELLA_URL)
-
+      # Create a method accessor for each of the server methods
       @server.call('system.listMethods').each do |m|
         if m.eql?("down") or m.eql?("up") or m.eql?("dequeue") or 
            m.eql?("last") or m.eql?("next") or m.eql?("force") or
@@ -16,7 +16,34 @@ class Hellanzb
           self.class.send(:define_method, m) {@server.call(m)}
         end
       end
-    rescue
+      # the hellanzb daemon has many status fields. Above we created the status method
+      # that returns all the values in a hash. Below we create specific accessors for each
+      # status value.
+      # List of methods created here:
+      #  queued_mb
+      #  percent_complete
+      #  time
+      #  total_dl_nzbs
+      #  is_paused
+      #  maxrate
+      #  rate
+      #  currently_downloading
+      #  currently_processing
+      #  total_dl_files
+      #  version
+      #  eta
+      #  config_file
+      #  hostname
+      #  queued
+      #  uptime
+      #  log_entries
+      #  total_dl_segments
+      #  total_dl_mb
+      
+      status.each do |s|
+        self.class.send(:define_method, s[0]) {@server.call("status")[s[0]]}
+      end
+    rescue Errno::ECONNREFUSED
       system(HELLA_BIN+" -c #{Merb.root}/config/hellanzb.conf -D")
       retry
     end
@@ -24,7 +51,7 @@ class Hellanzb
     @config_file ||= self.status['config_file']
     get_settings
   end
-  
+ 
   private
   def get_settings
     File.open(self.config_file, 'r').readlines.each do |line|
